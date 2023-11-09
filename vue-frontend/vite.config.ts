7ce162e4be4b29e78f5ef130df5517458c86b5e4
davq23 <davidquinterogranadillo@gmail.com>
2023-11-09 04:24:56 -0400

    Upgrade dependencies + fix base  url logic

--- vue-frontend/vite.config.ts
+++ vue-frontend/vite.config.ts
@@ -1,14 +1,30 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
+import { loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+
+
+export default ({ mode }: any) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    const FRONTEND_ASSETS_URL = process.env.VITE_FRONTEND_ASSETS_URL || '';
+
+
+    // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+    // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+    // https://vitejs.dev/config/
+    return defineConfig({
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            },
         },
-    },
-});
+        base: FRONTEND_ASSETS_URL,
+    });
+}
+
+
